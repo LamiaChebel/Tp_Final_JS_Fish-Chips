db1591746e422580f0702a8ea50fbdfb41d2fec2
LamiaChebel <lamia.chebel@3wa.io>
2023-04-11 16:15:40 +0200

    injection_dom index.js

--- assets/js/data/index.js
+++ assets/js/data/index.js
@@ -1,27 +1,27 @@
 export const datas = {
     section: {
         title: ["Today's ", "Special"],
-        crustyFish : {
+        crustyFish: {
             title: "The Crusty fish",
-            img: {url : "../img/recipe1.jpg", alt: "fresh fries fish with some green pees and a white sauce"},
+            img: { url: "./assets/img/recipe1.jpg", alt: "fresh fries fish with some green pees and a white sauce" },
             para: [
-                {p1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
-                {p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
-                {p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
+                { p1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?", },
+                { p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?", },
+                { p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?", },
             ],
             a: "Read more",
         },
-        multiFish : {
+        multiFish: {
             title: "The Multi fish",
-            img: {url : "./img/recipe2.jpg", alt: "crevettes, coquillages in different white cup and a green sauce"},
+            img: { url: "./assets/img/recipe2.jpg", alt: "crevettes, coquillages in different white cup and a green sauce" },
             para: [
-                {p1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
-                {p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
-                {p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
+                { p1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?", },
+                { p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?", },
+                { p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?", },
             ],
             a: "Read more",
         },
-        
+
     },
-        
+
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -21,34 +21,26 @@ function domHandlerSection() {
 
     /*Initialisation d'un tableau de l'objet section dans datas*/
     const data = Object.entries(datas.section);
-    // console.log(data[2].at(1));
 
     injectedSectionElement("h2").textContent = `${data[0].at(1).join(" ")}`;
 
     /*Boucle sur le tableau de data pour récupérer les données*/
 
-    for (let i = 0; i < data.length-1; i++) {
+    for (let i = 0; i < data.length - 1; i++) {
 
         injectedSectionElement("article").setAttribute("class", `${data[i + 1].at(0)}`);
-        injectedArticleElement(`.${data[i + 1].at(0)}`,"h3").textContent = `${data[i + 1].at(1).title}`;
-        injectedArticleElement(`.${data[i + 1].at(0)}`,"img");
-        document.querySelector("img").src = `${data[i + 1].at(1).img.url}`;
-        document.querySelector("img").alt = `${data[i + 1].at(1).img.alt}`;
+        injectedArticleElement(`.${data[i + 1].at(0)}`, "h3").textContent = `${data[i + 1].at(1).title}`;
+        let imageDatas = injectedArticleElement(`.${data[i + 1].at(0)}`, "img");
+        imageDatas.setAttribute("src", data[i + 1].at(1).img.url);
+        imageDatas.setAttribute("alt", data[i + 1].at(1).img.alt);
 
-        /*Boucle sur le tableau de para */
 
-        // for (let x = 0; x < data[i + 1].at(1).para.length; x++) {
-        //     for (const par of data[i + 1].at(1).para) {
-        //         console.log(data[i + 1].at(1).para[0].p${x});
-        //         injectedArticleElement("p").textContent = `${data[i + 1].at(1).para[x].par} `;
-        //     }
-        // }
-        console.log(data[i + 1].at(1).para[i].p2)
-        injectedArticleElement(`.${data[i + 1].at(0)}`,"p").textContent = `${data[i + 1].at(1).para[0].p1} `;
-        injectedArticleElement(`.${data[i + 1].at(0)}`,"p").textContent = `${data[i + 1].at(1).para[0].p2} `;
-        injectedArticleElement(`.${data[i + 1].at(0)}`,"p").textContent = `${data[i + 1].at(1).para[0].p3} `;
+        injectedArticleElement(`.${data[i + 1].at(0)}`, "p").textContent = `${data[i + 1].at(1).para[0].p1} `;
+        injectedArticleElement(`.${data[i + 1].at(0)}`, "p").textContent = `${data[i + 1].at(1).para[1].p2} `;
+        injectedArticleElement(`.${data[i + 1].at(0)}`, "p").textContent = `${data[i + 1].at(1).para[2].p3} `;
 
-        injectedArticleElement(`.${data[i + 1].at(0)}`,"a").textContent = `${data[+1].at(1).a} `;
+        injectedArticleElement(`.${data[i + 1].at(0)}`, "a").textContent = `${data[+1].at(1).a} `;
+
     }
 
 }
@@ -57,5 +49,6 @@ function domHandlerSection() {
 document.addEventListener("DOMContentLoaded", function () {
 
     domHandlerSection();
+    
 
 })
