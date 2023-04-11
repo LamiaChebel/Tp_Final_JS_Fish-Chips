a1576116483433be2d58b3fb0dfb7db9911fe03c
LamiaChebel <lamia.chebel@3wa.io>
2023-04-11 15:39:34 +0200

    update index.js

--- assets/js/data/index.js
+++ assets/js/data/index.js
@@ -3,7 +3,7 @@ export const datas = {
         title: ["Today's ", "Special"],
         crustyFish : {
             title: "The Crusty fish",
-            img: {url : "./img/recipe1.jpg", alt: "fresh fries fish with some green pees and a white sauce"},
+            img: {url : "../img/recipe1.jpg", alt: "fresh fries fish with some green pees and a white sauce"},
             para: [
                 {p1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
                 {p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
 import { datas } from "../js/data/index.js";
-console.log(datas);
 /** Fonction permettant d'injecter un élement dans le DOM
  * @param element 
  */
@@ -13,25 +12,43 @@ function injectedSectionElement(element) {
  * 
  * @param element 
  */
-function injectedArticleElement(element) {
-    return document.querySelector("article")
+function injectedArticleElement(article, element) {
+    return document.querySelector(article)
         .appendChild(document.createElement(element));
 }
 
 function domHandlerSection() {
 
-    injectedSectionElement("h2").textContent = `${datas.section.title.join(" ")}`;
+    /*Initialisation d'un tableau de l'objet section dans datas*/
+    const data = Object.entries(datas.section);
+    // console.log(data[2].at(1));
 
-    for (let i in datas.section) {
-        console.log(datas.section[i+1]);
-        injectedSectionElement("article").setAttribute("class", `${datas.section[i + 1]}`);
-        injectedArticleElement("h3").textContent = `${datas.section[i + 1].title}`;
-        injectedArticleElement("img").textContent = `${datas.section[i + 1].img}`;
-        for (const par of datas.section.para) {
-            injectedArticleElement("p").textContent = `${datas.section[i + 1].para[par]}`;
-        }
-        injectedArticleElement("a").textContent = `${datas.section[i + 1].a}`;
+    injectedSectionElement("h2").textContent = `${data[0].at(1).join(" ")}`;
 
+    /*Boucle sur le tableau de data pour récupérer les données*/
+
+    for (let i = 0; i < data.length-1; i++) {
+
+        injectedSectionElement("article").setAttribute("class", `${data[i + 1].at(0)}`);
+        injectedArticleElement(`.${data[i + 1].at(0)}`,"h3").textContent = `${data[i + 1].at(1).title}`;
+        injectedArticleElement(`.${data[i + 1].at(0)}`,"img");
+        document.querySelector("img").src = `${data[i + 1].at(1).img.url}`;
+        document.querySelector("img").alt = `${data[i + 1].at(1).img.alt}`;
+
+        /*Boucle sur le tableau de para */
+
+        // for (let x = 0; x < data[i + 1].at(1).para.length; x++) {
+        //     for (const par of data[i + 1].at(1).para) {
+        //         console.log(data[i + 1].at(1).para[0].p${x});
+        //         injectedArticleElement("p").textContent = `${data[i + 1].at(1).para[x].par} `;
+        //     }
+        // }
+        console.log(data[i + 1].at(1).para[i].p2)
+        injectedArticleElement(`.${data[i + 1].at(0)}`,"p").textContent = `${data[i + 1].at(1).para[0].p1} `;
+        injectedArticleElement(`.${data[i + 1].at(0)}`,"p").textContent = `${data[i + 1].at(1).para[0].p2} `;
+        injectedArticleElement(`.${data[i + 1].at(0)}`,"p").textContent = `${data[i + 1].at(1).para[0].p3} `;
+
+        injectedArticleElement(`.${data[i + 1].at(0)}`,"a").textContent = `${data[+1].at(1).a} `;
     }
 
 }
