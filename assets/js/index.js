87752118527aa331f14c1053af80f3b4f9bb64a1
LamiaChebel <lamia.chebel@3wa.io>
2023-04-12 04:31:47 +0200

    update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,8 +14,7 @@ function injectedSectionElement(element) {
  * @param article
  */
 function injectedArticleElement(article, element) {
-    return document.querySelector(article)
-        .appendChild(document.createElement(element));
+    return document.querySelector(article).appendChild(document.createElement(element));
 }
 
 /**Fonction permettant d'injecter la section avec ses enfants en utilisant l'objet datas
@@ -23,31 +22,44 @@ function injectedArticleElement(article, element) {
  */
 function domHandlerSection() {
 
-    // document.querySelector("section:nth-child(2)").classList.add
     /*Initialisation d'un tableau de l'objet section dans datas*/
     const data = Object.entries(datas.section);
+    // injectedSectionElement("h2").textContent = `${data[0].at(1)[1]}`;
+    // document.querySelector("h2")
+    injectedSectionElement("h2").append(document.createElement("span"), `${data[0].at(1)[1]}`);
+    document.querySelector("span").textContent = `${data[0].at(1)[0]}`;
+    document.querySelector("span").setAttribute("class", "secondary-color");
+    // const icon = [document.createElement("icon"), document.createElement("icon")];
+    // for (const ic of icon) {
+    //     ic.setAttribute("class", "fa-solid fa-droplet");
+    //     ic.setAttribute("aria-hidden", "true");
 
-    injectedSectionElement("h2").textContent = `${data[0].at(1).join(" ")}`;
+    // }
 
     /*Boucle sur le tableau de data pour récupérer les données*/
 
     for (let i = 0; i < data.length - 1; i++) {
 
-        injectedSectionElement("article").setAttribute("class", `${data[i + 1].at(0)}`);
-        injectedArticleElement(`.${data[i + 1].at(0)}`, "h3").textContent = `${data[i + 1].at(1).title}`;
-        let imageDatas = injectedArticleElement(`.${data[i + 1].at(0)}`, "img");
+        injectedSectionElement("article").id = `${data[i + 1].at(0)}`;
+        injectedArticleElement(`#${data[i + 1].at(0)}`, "h3")
+            .innerHTML = `<i class="fa-solid fa-droplet" aria-hidden = "true"></i>
+                        ${data[i + 1].at(1).title}
+                        <i class="fa-solid fa-droplet" aria-hidden = "true"></i>`;
+        // injectedArticleElement(`#${data[i + 1].at(0)}`, "h3").append(icon[0],`${data[i + 1].at(1).title}`,icon[1]) ;
+        let imageDatas = injectedArticleElement(`#${data[i + 1].at(0)}`, "img");
         imageDatas.setAttribute("src", data[i + 1].at(1).img.url);
         imageDatas.setAttribute("alt", data[i + 1].at(1).img.alt);
 
 
-        injectedArticleElement(`.${data[i + 1].at(0)}`, "p").textContent = `${data[i + 1].at(1).para[0].p1} `;
-        injectedArticleElement(`.${data[i + 1].at(0)}`, "p").textContent = `${data[i + 1].at(1).para[1].p2} `;
-        injectedArticleElement(`.${data[i + 1].at(0)}`, "p").textContent = `${data[i + 1].at(1).para[2].p3} `;
+        injectedArticleElement(`#${data[i + 1].at(0)}`, "p").textContent = `${data[i + 1].at(1).para[0].p1} `;
+        injectedArticleElement(`#${data[i + 1].at(0)}`, "p").textContent = `${data[i + 1].at(1).para[1].p2} `;
+        injectedArticleElement(`#${data[i + 1].at(0)}`, "p").textContent = `${data[i + 1].at(1).para[2].p3} `;
 
-        injectedArticleElement(`.${data[i + 1].at(0)}`, "a").textContent = `${data[i + 1].at(1).a} `;
+        injectedArticleElement(`#${data[i + 1].at(0)}`, "a").textContent = `${data[i + 1].at(1).a} `;
 
     }
 
+    document.querySelector("section:nth-child(2)").n;
 }
 
 /**Fonction utilitaire de cache-cache pour les éléments
@@ -68,7 +80,7 @@ function switchDisplayHide(element, valueDisplayInitial, valueDisplay) {
 function onClickDisplayHide(e) {
     e.preventDefault();
     if (e.target.id === "btn-section") {
-        switchDisplayHide("section:nth-child(2)", "", "initial");
+        switchDisplayHide("section:nth-child(2)", "", "flex");
     }
     else {
         switchDisplayHide("aside", "none", "inline-block");
